64a73726951be54b495570e802c8515cda72a549
thaihuynhxyz <thaihuynhxyz@gmail.com>
2019-09-05 00:40:02 +0700

    Add fontawesome

--- lib/main_view.dart
+++ lib/main_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:licham/main_bloc.dart';
 import 'package:licham/main_state.dart';
+import 'package:licham/widget/my_flutter_app_icons.dart';
 
 class MainView extends StatefulWidget {
   @override
@@ -31,6 +32,26 @@ class _MainState extends State<MainView> {
               title: Text('Title'),
             ),
             body: Text('Body'),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerDocked,
+            floatingActionButton: FloatingActionButton(
+              child: Icon(CustomIcons.calendar_plus_o),
+              onPressed: () => {},
+            ),
+            bottomNavigationBar: BottomAppBar(
+                shape: CircularNotchedRectangle(),
+                notchMargin: 4.0,
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      IconButton(
+                        icon: Icon(CustomIcons.down_circled2),
+                        onPressed: () => {},
+                      ),
+                      IconButton(
+                          icon: Icon(CustomIcons.github_circled),
+                          onPressed: () {})
+                    ])),
           );
         },
       ),
